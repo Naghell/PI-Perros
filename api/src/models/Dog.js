75907d8abc70dfe40c6e70ab36443488c81223f5
Renzo Battaglino <naghybusiness@gmail.com>
2023-06-04 23:38:30 -0300

    Models + inicio controllers

--- api/src/models/Dog.js
+++ api/src/models/Dog.js
@@ -1,11 +1,32 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define('dog', {
-    name: {
+  sequelize.define("dog", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    imagen: {
       type: DataTypes.STRING,
+      allowNull: false,
+    },
+    nombre: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    altura: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    peso: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    anios_vida: {
+      type: DataTypes.INTEGER,
       allowNull: false,
     },
   });
