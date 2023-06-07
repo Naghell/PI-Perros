c052a82e61dffeb9ea5f0b3c1515a40e5c9ea5b0
Renzo Battaglino <naghybusiness@gmail.com>
2023-06-06 21:48:37 -0300

    Landing terminado

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,9 +1,18 @@
-import styles from './Landing.module.css'
+import styles from './Landing.module.css';
+import { useEffect } from 'react';
 
 const Landing = () => {
+    const landingJoin = () => {
+        const audio = new Audio("./intro.mp3");
+        audio.play();
+    }
+
     return(
-        <main className={styles.background}>
-            <h1>Test</h1>
+        <main className={styles.landing__background}>
+            <div className={styles.landing__container}>
+                <img alt="Logo" className={styles.landing__image} src='/logo.svg'></img>
+                <button onClick={landingJoin} className={styles.landing__button}>INGRESAR</button>
+            </div>
         </main>
     )
 }
