aff040c636cc734f3cf211513580daab9b86bb5d
Renzo Battaglino <naghybusiness@gmail.com>
2023-06-20 12:53:14 -0300

    404 terminado

--- client/src/components/NotFound/NotFound.jsx
+++ client/src/components/NotFound/NotFound.jsx
@@ -1,7 +1,15 @@
+import style from './NotFound.module.css'
+import { Link } from 'react-router-dom'
+
 const NotFound = () => {
     return(
-        <div>
+        <div className={style.notfound__container}>
             <h1>404</h1>
+            <h2>Ups! Parece que estás perdido...</h2>
+
+            <Link to="/home">
+                <button className={style.notfound__button}>Volver al inicio</button>
+            </Link>
         </div>
     )
 }
